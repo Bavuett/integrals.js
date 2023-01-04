eb0cd1388144d3868229357b8d49cf7c3244948f
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-01-04 15:44:59 +0100

    🖊️ React App setup.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React!</h1>
+    </div>
+  );
+}
+
+export default App;
